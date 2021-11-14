e09bcde8ff13e7ba28ea683c49e919ff18a5bd07
Csaba Schmidtmayer <cs.schmidtmayer@gmail.com>
2021-11-14 21:55:36 +0100

    feat: Add logic to ProductListItem

--- src/app/product-list-item/product-list-item.component.ts
+++ src/app/product-list-item/product-list-item.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 
+import Product from '../models/product';
+
 @Component({
   selector: 'app-product-list-item',
   templateUrl: './product-list-item.component.html',
   styleUrls: ['./product-list-item.component.css']
 })
 export class ProductListItemComponent implements OnInit {
+  product: Product = {
+    name: '',
+    price: 0,
+    imageUrl: ''
+  };
+  quantity: number = 0;
 
-  constructor() { }
+  constructor() {
+    this.product = {
+      name: 'Book',
+      price: 9.99,
+      imageUrl: 'https://images.unsplash.com/photo-1544947950-fa07a98d237f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80'
+    }
+  }
 
   ngOnInit(): void {
   }
 
+  addToCart(): void {
+    console.log(`${this.quantity} pieces of ${this.product.name} added to the cart`);
+  }
 }
