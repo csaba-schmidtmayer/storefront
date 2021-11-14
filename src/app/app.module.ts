88dcb511e4476ff9b00806c9fe955a3cb2b802b6
Csaba Schmidtmayer <cs.schmidtmayer@gmail.com>
2021-11-14 20:33:29 +0100

    feat: Add ProductListItem component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductListItemComponent } from './product-list-item/product-list-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListItemComponent
   ],
   imports: [
     BrowserModule,
