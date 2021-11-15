09522a9b0ead3f6bf420d35fde387c650c518b0f
Csaba Schmidtmayer <cs.schmidtmayer@gmail.com>
2021-11-15 10:33:05 +0100

    feat: Add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductItemDetailComponent } from './product-item-detail/product-item-detail.component';
+
+const routes: Routes = [
+  { path: '', component: ProductListComponent },
+  //{ path: 'cart', component: CartComponent },
+  { path: 'products/:id', component: ProductItemDetailComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
