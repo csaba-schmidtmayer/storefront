cd0a939b7975fdce00f61336c08e28a26df110d6
Csaba Schmidtmayer <cs.schmidtmayer@gmail.com>
2021-11-15 10:26:29 +0100

    feat: Fill out ProductItemDetail

--- src/app/product-item-detail/product-item-detail.component.ts
+++ src/app/product-item-detail/product-item-detail.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+import { ManageCartService } from '../manage-cart.service';
+
+import Product from '../models/product';
 
 @Component({
   selector: 'app-product-item-detail',
@@ -6,10 +10,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-item-detail.component.css']
 })
 export class ProductItemDetailComponent implements OnInit {
+  @Input() product!: Product;
+  quantity: number = 0;
 
-  constructor() { }
+  constructor(private cartService: ManageCartService) {
+    this.product = {
+      "id": 1,
+      "name": "Book",
+      "price": 9.99,
+      "imageUrl": "https://images.unsplash.com/photo-1544947950-fa07a98d237f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80",
+      "description": "You can read it!"
+    };
+  }
 
   ngOnInit(): void {
   }
 
+  addToCart(): void {
+    this.cartService.addToCart(this.product, this.quantity);
+  }
 }
