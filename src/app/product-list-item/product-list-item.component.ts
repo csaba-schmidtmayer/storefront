dfb53114aae3ad4bfd449c9de25962515e359052
Csaba Schmidtmayer <cs.schmidtmayer@gmail.com>
2021-11-15 09:14:58 +0100

    feat: Hook up ProductList

--- src/app/product-list-item/product-list-item.component.ts
+++ src/app/product-list-item/product-list-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 import Product from '../models/product';
 
@@ -8,19 +8,10 @@ import Product from '../models/product';
   styleUrls: ['./product-list-item.component.css']
 })
 export class ProductListItemComponent implements OnInit {
-  product: Product = {
-    name: '',
-    price: 0,
-    imageUrl: ''
-  };
+  @Input() product!: Product;
   quantity: number = 0;
 
   constructor() {
-    this.product = {
-      name: 'Book',
-      price: 9.99,
-      imageUrl: 'https://images.unsplash.com/photo-1544947950-fa07a98d237f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80'
-    }
   }
 
   ngOnInit(): void {
