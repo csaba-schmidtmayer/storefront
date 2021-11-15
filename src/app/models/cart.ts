a6779a822e307e064081da9f3e3e95aed6a3cdc4
Csaba Schmidtmayer <cs.schmidtmayer@gmail.com>
2021-11-15 09:23:20 +0100

    feat: Add type definition for Cart

---
+++ src/app/models/cart.ts
@@ -0,0 +1,15 @@
+import Product from './product';
+
+type CartItem = {
+  product: Product,
+  quantity: number
+};
+
+type Cart = {
+  products: {
+    [id: number]: CartItem
+  } | null,
+  cartValue: number
+};
+
+export default Cart;
