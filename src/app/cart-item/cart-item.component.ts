607e81ca24c6aa2a190d85c4ce9abf0f289f226d
Csaba Schmidtmayer <cs.schmidtmayer@gmail.com>
2021-11-16 08:35:12 +0100

    feat: Implement CartItem

--- src/app/cart-item/cart-item.component.ts
+++ src/app/cart-item/cart-item.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+import Product from '../models/product';
 
 @Component({
   selector: 'app-cart-item',
@@ -6,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart-item.component.css']
 })
 export class CartItemComponent implements OnInit {
+  @Input() product!: Product;
+  quantity: number = 0;
 
   constructor() { }
 
